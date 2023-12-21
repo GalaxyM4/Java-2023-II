0471f1a3751d16b9a1debb3c04de8aecae6375c4
Arturo Marín <contact.artmarin401@gmail.com>
2023-12-21 12:14:02 -0500

    Luchito de ejemplo

---
+++ src/sem_2/MainPersonita.java
@@ -0,0 +1,15 @@
+package Fracciones;
+
+import Algoritmos.Persona;
+
+public class mainP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Persona p1 = new Persona("Luis el más activo", 19); 
+		p1.mostrar();
+		
+	}
+
+}
