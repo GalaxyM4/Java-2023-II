b63dae7abfccca69b1324c0208773f772544642e
Arturo Marín <contact.artmarin401@gmail.com>
2023-12-21 11:50:42 -0500

    viva luis

---
+++ src/sem_2/MainFraccion.java
@@ -0,0 +1,15 @@
+package Fracciones;
+
+public class MainF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frac1 f= new Frac1();
+		f.numerador=5;
+		f.denominador=0;
+		
+		System.out.println(f.numerador + "/" + f.denominador);
+		
+	}
+
+}
