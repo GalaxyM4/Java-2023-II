abbd151c5ea2e91bc36c51182711394a471e09cd
Arturo Marín <contact.artmarin401@gmail.com>
2023-12-21 11:38:28 -0500

    Hola, editalo si falta algo

--- src/sem_2/Persona1.java
+++ src/sem_2/Persona1.java
@@ -1,1 +1,19 @@
+package Algoritmos;
+
+public class Persona {
+	public String nombre;
+	public int edad;
+	
+	public Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	} 
+	
+	public void mostrar() {
+		System.out.println("El nombre es: "+nombre); 
+		System.out.println("La edad es: "+edad);
+	}
+
+}
 
