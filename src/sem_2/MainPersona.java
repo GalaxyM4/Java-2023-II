4745e80d4c52803b1c1acf146130e5005ddd820e
Arturo Marín <contact.artmarin401@gmail.com>
2023-12-21 11:42:03 -0500

    Faltan vainas acá

---
+++ src/sem_2/MainPersona.java
@@ -0,0 +1,12 @@
+package Algoritmos;
+
+public class Main {
+ public String nombre;
+ public int edad; 
+ 
+ public static void main(String[] args) {
+ 
+	 Persona p1 = new Persona ("Imanol", 18);
+	 p1.mostrar();
+ }
+ }
