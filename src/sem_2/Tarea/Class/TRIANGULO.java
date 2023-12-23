b71bb5fad4d7026f56fe16933eb8844eeb901a1b
Arturo Marín <contact.artmarin401@gmail.com>
2023-12-22 20:52:59 -0500

    Edita el Package al sem_2 thx

---
+++ src/sem_2/Tarea/Class/TRIANGULO.java
@@ -0,0 +1,50 @@
+package Tarea;
+
+public class TRIANGULO {
+
+		private String Pololo;
+		private int l1;
+		private int l2;
+		private int l3;
+		public TRIANGULO(String pololo, int l1, int l2, int l3) {
+			super();
+			Pololo = pololo;
+			this.l1 = l1;
+			this.l2 = l2;
+			this.l3 = l3;
+		}
+		public String getPololo() {
+			return Pololo;
+		}
+		public void setPololo(String pololo) {
+			Pololo = pololo;
+		}
+		public int getL1() {
+			return l1;
+		}
+		public void setL1(int l1) {
+			this.l1 = l1;
+		}
+		public int getL2() {
+			return l2;
+		}
+		public void setL2(int l2) {
+			this.l2 = l2;
+		}
+		public int getL3() {
+			return l3;
+		}
+		public void setL3(int l3) {
+			this.l3 = l3;
+		}
+		
+		public void consola() {
+			
+			double semip = (l1+l2+l3) / 2;
+			
+			System.out.println(Pololo+"El Resultado es: ");
+			System.out.println("El área es: " + Math.sqrt(semip * (semip - l1) * (semip - l2) * (semip - l3)));
+			System.out.println("El perímetro es: "+ (l1+l2+l3));
+		}
+}
+	
