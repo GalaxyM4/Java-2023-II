f5800f63911eb9f8135ac983f6991e912e74804f
Luis <luisirigoin123@gmail.com>
2024-01-02 22:28:31 -0500

    propiedades privadas

--- src/sem_3/Tarea/Class/Nagazaki.java
+++ src/sem_3/Tarea/Class/Nagazaki.java
@@ -1,17 +1,38 @@
 package sem_3.Tarea.Class;
 
 public class Nagazaki extends City {
+    private String tourist_place;
+    private String mayor;
     public Nagazaki() {
         super();
         this.name = "Nagazaki";
         this.population = 429508l;
         this.area = 405.9f;
+        this.mayor = "Shirō Suzuki";
+        this.tourist_place = "El Parque de la Paz";
+    }
+    //get set
+    public String getTourist_place() {
+        return tourist_place;
+    }
+
+    public void setTourist_place(String tourist_place) {
+        this.tourist_place = tourist_place;
+    }
+
+    public String getMayor() {
+        return mayor;
+    }
+
+    public void setMayor(String mayor) {
+        this.mayor = mayor;
     }
 
     @Override
     public void showAll() {
         System.out.println(" --------------------------- Nagazaki --------------------------- ");
         System.out.println(name + " ciudad de Japón, con una población de " + population + ", y una extensión de " + area + " km²");
+        System.out.println("Con " + mayor + " de alcalde y " + tourist_place + " como su lugar turístico. ");
     }
 
     public void nuclear() throws InterruptedException {
