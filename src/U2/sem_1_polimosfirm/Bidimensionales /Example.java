146578e6630636aa410203f6e2f84726875da630
Arturo Marín <contact.artmarin401@gmail.com>
2024-01-18 12:26:29 -0500

    xd

---
+++ src/U2/sem_1_polimosfirm/Bidimensionales /Example.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class LeerArreglos {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Número de elementos");
+		int n=sc.nextInt();
+		int [] camilo = new int[n];
+
+		// Leer vector
+
+		// for(int i=0; i<camilo.length; i++) {
+		//System.out.printf("c[" + (i+1) +"]");
+		//camilo[i] = sc.nextInt();
+		// }
+
+		// Generar el Vector
+		for(int i=0; i<camilo.length; i++) { 
+			camilo[i] = (int)(Math.random()*50);
+		}
+
+		// Mostrar el vector
+
+		for(int i=0; i<camilo.length; i++) {
+			System.out.print(camilo[i] + "\t");
+		}
+		System.out.println();
+
+		sc.close();
+
+	}
+}
+
