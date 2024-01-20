9b7d72be65c82f9c389b1bd3f7b7faeea7e2f04d
Luis <luisirigoin123@gmail.com>
2024-01-20 08:46:59 -0500

    Ejercicio cartas hecho

--- src/U2/sem_2_arrays2/Tarea/Cards/Card.java
+++ src/U2/sem_2_arrays2/Tarea/Cards/Card.java
@@ -1,5 +1,21 @@
 package U2.sem_2_arrays2.Tarea.Cards;
 
+import Utils.PrintColor;
+
 public class Card {
-    
+    int number;
+    String symbol;
+
+    public Card(int num, String symbol) {
+        this.number = num;
+        this.symbol = symbol;
+    }
+
+    public String toString() {
+        if (symbol == DeckCards.symbols[2] || symbol == DeckCards.symbols[3]) {
+            return PrintColor.RED + this.number + " " + this.symbol + PrintColor.RESET;
+        }else {
+            return this.number + " " + this.symbol;
+        }
+    }
 }
---
+++ src/U2/sem_2_arrays2/Tarea/Cards/DeckCards.java
@@ -0,0 +1,45 @@
+package U2.sem_2_arrays2.Tarea.Cards;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class DeckCards {
+    public Card[] deck;
+    public static String[] symbols = new String[]{"=>", "*", "<3", "<>"};
+
+    public DeckCards() {
+        System.out.println("Gerando mazo...");
+        this.generate_cards();
+    }
+
+    /**
+     * Este método genera las cartas en orden.
+     */
+    private void generate_cards() {
+        deck = new Card[13*4];
+        for (int i = 0; i < symbols.length; i++) {
+            var symb = symbols[i];
+            for (int j = 0; j < 13; j++) {
+                deck[j+(13*i)] = new Card(j+1, symb);
+            }
+        }
+    }
+
+    public void shuffle_cards() {
+        if(deck.length == 0) {
+            throw new Error("El mazo no está generado.");
+        }
+        List<Card> list_cards = Arrays.asList(deck);
+        Collections.shuffle(list_cards);
+        list_cards.toArray(deck);
+    }
+
+    public void printCards() { //Muestra todas las cartas en el orden de la lista.
+        if(deck.length == 0) {
+            throw new Error("El mazo no está generado.");
+        }
+        for (Card card : deck) {
+            System.out.println(card);
+        }
+    }
+}
