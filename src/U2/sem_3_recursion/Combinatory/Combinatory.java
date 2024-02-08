7ec870cc4c53d431ece81ebb8cde161b1889775f
Luis <luisirigoin123@gmail.com>
2024-02-08 16:25:38 -0500

    Scanner implemented

--- src/U2/sem_3_recursion/Combinatory/Combinatory.java
+++ src/U2/sem_3_recursion/Combinatory/Combinatory.java
@@ -1,7 +1,20 @@
 package U2.sem_3_recursion.Combinatory;
+import java.util.Scanner;
 import U2.sem_3_recursion.Factorial.FactorialFunction;
 
 public class Combinatory {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingresa la longitud de la lista.");
+        Integer[] list = new Integer[input.nextInt()];
+        System.out.println("Ingresa el tamaño de los grupos.");
+        int group = input.nextInt();
+        input.close();
+        ///
+        System.out.println("Las combinaciones posibles son: " + comb_elements(group, list));
+
+    }
+
     public static <T> int comb_elements(int group, T[] list) {
         int n = list.length;
         int g = group;
